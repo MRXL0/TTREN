a88a9ff46c0e6078a1bbfd566491e0a8c072cb4b
renxiaolong <shadyme@163.com>
2021-12-07 16:45:47 +0800

    f

---
+++ Example/Pods/Target Support Files/Pods-TTREN_Tests/Pods-TTREN_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TTREN_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TTREN_Tests
+@end
---
+++ Example/Pods/Target Support Files/TTREN/TTREN-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TTREN : NSObject
+@end
+@implementation PodsDummy_TTREN
+@end
