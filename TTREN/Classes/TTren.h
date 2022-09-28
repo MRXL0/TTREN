8654853ee2d7407ce1179f5060bc2ae8fd38f8ea
iOS2 <shadyme@163.com>
2022-09-28 15:31:00 +0800

    0.2.2

---
+++ TTREN/Classes/TTren.h
@@ -0,0 +1,16 @@
+//
+//  TTren.h
+//  TTREN
+//
+//  Created by iOS2 on 2022/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTren : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TTREN/Classes/TTren.m
@@ -0,0 +1,12 @@
+//
+//  TTren.m
+//  TTREN
+//
+//  Created by iOS2 on 2022/9/28.
+//
+
+#import "TTren.h"
+
+@implementation TTren
+
+@end
